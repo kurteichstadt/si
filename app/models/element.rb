b8a1e6dfc1eec48cf7bd48d3966778415496343a
justin.sabelko <justin.sabelko@68e5226f-b13b-0410-b3eb-f6601b5927d1>
2007-11-08 18:12:16 +0000

    Allow for limit to be called while on admin side
    git-svn-id: http://svn.uscm.org/si/trunk@48 68e5226f-b13b-0410-b3eb-f6601b5927d1

--- app/models/element.rb
+++ app/models/element.rb
@@ -4,10 +4,14 @@ require_dependency RAILS_ROOT + "/vendor/plugins/questionnaire/app/models/elemen
 class Element < ActiveRecord::Base
   def limit(application)
     if !self.nil? and !self.object_name.nil? and !self.attribute_name.nil? and !self.object_name.blank? and !self.attribute_name.blank?
-      unless eval("application." + self.object_name + ".nil?")
-        klass = eval("application." + self.object_name + ".class")
-        column = klass.columns_hash[self.attribute_name]
-        column.limit
+      begin
+        unless eval("application." + self.object_name + ".nil?")
+          klass = eval("application." + self.object_name + ".class")
+          column = klass.columns_hash[self.attribute_name]
+          column.limit
+        end
+      rescue
+        nil
       end
     end
   end
