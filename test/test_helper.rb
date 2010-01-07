fc572cf655faf7ea9d7da7aa236d3f4c337e014f
Josh Starcher <josh.starcher@gmail.com>
2010-01-07 11:30:51 -0600

    update CE

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,19 +32,6 @@ class ActiveSupport::TestCase
     @apply.apply_sheets.create(:sleeve_sheet => @sleeve_sheet, :answer_sheet => @answer_sheet)
   end
   
-  def setup_with_hr_si_application
-    @sleeve = Factory(:sleeve)
-    @address = Factory(:address, :person => @person)
-    @hr_si_application = Factory(:hr_si_application, :person => @person)
-    @apply = @hr_si_application.apply
-    @apply.update_attribute(:sleeve, @sleeve) if @apply.sleeve.nil?
-    @sleeve_sheet = Factory(:sleeve_sheet, :sleeve => @sleeve)
-    @question_sheet = @sleeve_sheet.question_sheet
-    @answer_sheet = @question_sheet.answer_sheets.create
-    Factory(:page, :question_sheet => @question_sheet)
-    @apply.apply_sheets.create(:sleeve_sheet => @sleeve_sheet, :answer_sheet => @answer_sheet)
-  end
-  
   def setup_reference(sleeve)
     @ref_sheet = Factory(:ref_sheet, :sleeve => sleeve)
     @ref_question_sheet = @ref_sheet.question_sheet
