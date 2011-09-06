2b419751a1370d0c9fc6149eae57b1c0668312d8
Justin Sabelko <justin.sabelko@uscm.org>
2011-09-06 15:15:51 -0400

    Open Application

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,7 +3,7 @@ class AccountController < ApplicationController
   skip_before_filter CAS::Filter 
   skip_before_filter AuthenticationFilter
   prepend_before_filter :login_from_cookie
-  before_filter :prod_check, :except => [:closed, :secret_hooey]
+  #before_filter :prod_check, :except => [:closed, :secret_hooey]
   filter_parameter_logging :password
 
   def index
