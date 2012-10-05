110ff752a1d956fdd41027f2ac0202a98225a4ba
Justin Sabelko <justin.sabelko@uscm.org>
2012-10-05 14:42:01 -0500

    Get rid of old models

--- app/models/deprecated_apply_sheet.rb
+++
@@ -1,9 +0,0 @@
-# links sheets
-class ApplySheet < ActiveRecord::Base
-  set_table_name "#{TABLE_NAME_PREFIX}#{self.table_name}"
-  
-  belongs_to :apply
-  belongs_to :sleeve_sheet    # back to question sheet and retitle
-  belongs_to :answer_sheet_question_sheet, :foreign_key => :answer_sheet_id
-  
-end
