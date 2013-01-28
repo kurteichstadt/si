b5c4000fea4c3a2b96f8efdc25050a4242847dcf
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-01-28 18:00:42 +0800

    Move session_secret to config.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Si::Application.config.secret_token = '3475adcb1bbe8441964f9d9a45d0941sadf8023ruopial;m0ec51e76d7b0easdfasfhoi2u6t0gijsdbmd81e38320f757f80cd4e6b49079da52f747125'
+Si::Application.config.secret_token = APP_CONFIG['session_secret']
