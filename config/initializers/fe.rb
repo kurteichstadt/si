3d9b1e32102002358ec8a0505b916a03737b080f
Andrew Roth <andrewroth@gmail.com>
2014-07-07 12:26:59 -0400

    forgot this for last commit

--- config/initializers/fe.rb
+++ config/initializers/fe.rb
@@ -2,10 +2,10 @@ TABLE_NAME_PREFIX = 'si_'
 module Fe
   # prefix for database tables
   mattr_accessor :table_name_prefix
-  self.table_name_prefix = TABLE_NAME_PREFIX
+  self.table_name_prefix = 'si_'
   
   mattr_accessor :answer_sheet_class
-  self.answer_sheet_class = 'Apply'
+  self.answer_sheet_class = 'Fe::Apply'
   
   mattr_accessor :from_email
   self.from_email = 'STINT and Internships <StintandInternships@uscm.org>'
