6be01589b10c51cb897d5f63ba4e78396255397c
Andrew Roth <andrewroth@gmail.com>
2014-07-30 13:21:57 -0400

    don't remove position, I can't find any migration that moved the position data to somewhere

--- db/migrate/20131031170017_remove_question_sheet_id_from_element.rb
+++ db/migrate/20131031170017_remove_question_sheet_id_from_element.rb
@@ -1,6 +1,5 @@
 class RemoveQuestionSheetIdFromElement < ActiveRecord::Migration
   def change
     remove_column :si_elements, :question_sheet_id
-    remove_column :si_elements, :position
   end
 end
