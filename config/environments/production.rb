0d4bf0185b07f91ab27d7f1433834037c7c29938
Josh Starcher <josh.starcher@gmail.com>
2014-08-03 17:43:27 -0400

    Remove config.threadsafe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,10 @@
 Si::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  
+
   # Code is not reloaded between requests
   config.cache_classes = true
   config.eager_load = true
-  
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -20,17 +20,17 @@ Si::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
- 
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
- 
+
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
- 
+
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
@@ -57,9 +57,6 @@ Si::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'stint.uscm.org' }
-
-  # Enable threaded mode
-  config.threadsafe!
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
